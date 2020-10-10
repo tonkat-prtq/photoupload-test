3925e368e7c1f4ed51cd4e1d72906d71c22e8ea0
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-10 22:01:16 +0900

    Add: 正しく追加されてるかどうか確認するため、foreachですべてのuser情報を$userに入れ、ユーザー名と画像を表示させるように

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,1 +1,8 @@
-<img src="{{ $user->file_path }}">
+@foreach($users as $user)
+<div class="user-name">
+  {{ $user->name }}
+</div>
+<div class="image">
+  <img src="{{ $user->file_path }}">
+</div>
+@endforeach
