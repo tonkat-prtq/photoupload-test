69147c166537f9a2fe368c21e4b6e6cbed148b43
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-11 14:12:43 +0900

    Add: migrate:fresh

--- database/migrations/2020_10_10_101137_add_file_path_to_users_table.php
+++ database/migrations/2020_10_10_101137_add_file_path_to_users_table.php
@@ -14,7 +14,7 @@ class AddFilePathToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('path');
+            $table->string('file_path');
         });
     }
 
