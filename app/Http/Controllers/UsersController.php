3dea34bfb579e38693748a478813a202f3b9f6b9
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-11 16:08:52 +0900

    Add: コメントを追加

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,7 +16,6 @@ class UsersController extends Controller
     public function index()
     {
         $users = User::all();
-        dd($users);
         return view('users.index', ['users' => $users]);
     }
 
@@ -38,20 +37,43 @@ class UsersController extends Controller
      */
     public function store(Request $request)
     {
+        // バリデーションをしている
         $request->validate([
             'name'=>['required','string','max:255'],
             'file_path'=>['file','mimes:jpeg,png,jpg,bmb','max:2048'],
         ]);
 
         $path = '';
+
+        // フォームから送られてきたパラメータに、file_pathがあれば それを変数$fileに入れて以下の処理を実行
         if($file = $request->file_path){
-            $path = $file->store('uploads');
+
+            // storeメソッド(サーバー上にアップロードしたファイルを保存するメソッド)でアップロード
+            // publicディスクのuploadsディレクトリに保存される
+            // 画像が保存されたディレクトリのパスを、変数$pathに入れている
+            $path = $file->store('uploads', 'public');
+
+            /*
+            [補足]
+            ディスクというのは、「ファイル保存の定義」
+            ディスクにはデフォルトで３つの定義がある(local, public, s3)
+            それぞれに、どのフォルダにどうやって保存するかってのが定義されている
+            その定義は、config/filesystem.phpにある
+            なにも指定しなければlocalが使われる
+            */
+            
         } 
 
         // dd($path, $request->file_path);
 
+        // App\User::でUserクラスのインスタンスを生成し、create()でDBへのInsertを実行している
+        // Userモデルのcreateメソッドを使ってDBにレコードを登録している
         \App\User::create([
+
+            // $request->input('name')で、ユーザーが入力した'name'の値を取り出して、Userテーブルの'name'カラムに入れる
             'name'=>$request->input('name'),
+
+            // 'file_path'カラムに$path(画像が保存されたディレクトリのパス)を入れる
             'file_path'=> $path,
         ]);
 
